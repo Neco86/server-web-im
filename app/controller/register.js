af364b39bf2b51db1aca528d8bb6a00dbd74f2d8
Neco86 <1157868866@qq.com>
2020-03-17 18:31:51 +0800

    fix:添加说明注释

--- app/controller/register.js
+++ app/controller/register.js
@@ -7,29 +7,37 @@ const { createRandomNum } = require('../utils/utils');
 
 
 class RegisterController extends Controller {
+  // 获取验证码
   async getCaptcha() {
     const { ctx, app } = this;
     const data = ctx.request.body;
     const { email } = data;
+    // 当前用户是否存在
     const exist = await app.mysql
       .query('SELECT * FROM userInfo WHERE email = ?', email);
     if (exist.length) {
+      // 存在
       ctx.body = {
         code: ERROR_CODE,
         msg: '用户存在,请尝试登陆',
       };
     } else {
+      // 不存在
       const captcha = createRandomNum(6);
       const timestamp = String(Date.now());
+      // 是否获取过验证码
       const exist = await app.mysql
         .query('SELECT * FROM confirm WHERE email = ?', email);
       if (exist.length) {
+        // 获取过
         await app.mysql
           .query('UPDATE confirm SET captcha = ?,timestamp = ? WHERE email = ?', [captcha, timestamp, email]);
       } else {
+        // 没获取过
         await app.mysql
           .query('INSERT INTO confirm(email,captcha,timestamp) VALUES(?,?,?)', [email, captcha, timestamp]);
       }
+      // 发送邮件
       const mail = {
         from: '<web_im@163.com>',
         subject: '注册ChatChat',
@@ -43,28 +51,37 @@ class RegisterController extends Controller {
       };
     }
   }
+  // 注册
   async register() {
     const { ctx, app } = this;
     const data = ctx.request.body;
     const { nickname, password, email, captcha } = data;
+    // 当前用户是否存在
     const exist = await app.mysql
       .query('SELECT * FROM userInfo WHERE email = ?', email);
     if (exist.length) {
+      // 存在
       ctx.body = {
         code: ERROR_CODE,
         msg: '用户存在,请尝试登陆',
       };
     } else {
+      // 不存在
+      // 验证码是否错误或过期
       const confirm = await app.mysql
         .query('SELECT * FROM confirm WHERE email = ?', email);
       if ((confirm[0].captcha !== captcha) || (Date.now() - confirm.timestamp > 30 * 60 * 1000)) {
+        // 错误或过期
         ctx.body = {
           code: ERROR_CODE,
           msg: '验证码错误,请尝试重新获取验证码',
         };
       } else {
+        // 验证码正确
+        // 删除获取验证码
         await app.mysql
           .query('DELETE FROM confirm where email = ?', email);
+        // 插入用户表
         await app.mysql
           .query('INSERT INTO userInfo(email,nickName,password) VALUES(?,?,?)', [email, nickname, password]);
         ctx.body = {
