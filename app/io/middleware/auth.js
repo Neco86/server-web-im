3e4798d91161d059fa7c1a9c50859eea562ce79e
Neco86 <1157868866@qq.com>
2020-03-25 19:25:22 +0800

    feat:上下线通知

--- app/io/middleware/auth.js
+++ app/io/middleware/auth.js
@@ -32,6 +32,25 @@ module.exports = () => {
       await app.mysql
         .query('UPDATE userInfo SET status = ? WHERE email = ?',
           [USER_STATUS.OFFLINE, tokenInfo.email]);
+      // 通知离线
+      const logoutUsers = await app.mysql
+        .query('SELECT * FROM userInfo WHERE email = ?', tokenInfo.email);
+      if (logoutUsers[0].recentStatus === USER_STATUS.ONLINE) {
+        for (let i = 0; i < friends.length; i++) {
+          const friend = friends[i];
+          const nsp = app.io.of('/');
+          if (friend.type === FRIEND_TYPE.FRIEND && nsp.sockets[friend.peer]) {
+            const myInfo = await app.mysql
+              .query(`SELECT * FROM userInfo WHERE email = '${socket.id}'`);
+            const friendInfo = await app.mysql
+              .query(`SELECT * FROM userChatInfo WHERE peer = '${socket.id}' AND email = '${friend.peer}'`);
+            const { email } = myInfo[0];
+            nsp.sockets[friend.peer].emit('offline', {
+              email, groupKey: friendInfo[0].groupKey,
+            });
+          }
+        }
+      }
     } catch (e) {
       socket.disconnect();
     }
